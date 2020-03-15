05d7913e708e3ad890947f91f790befcd4ebd292
WayupKG <51049379+WayupKG@users.noreply.github.com>
2020-03-15 14:29:17 +0600

    Task4

---
+++ Task4.py
@@ -0,0 +1,9 @@
+class __List__:
+    def __init__(self, all_contacts):
+        self.all_contacts = []
+     
+
+class ContactList(__List__):
+    def __init__(self):
+        # __List__.__init__("asd", "asdas", "asdaasd")
+        pass
